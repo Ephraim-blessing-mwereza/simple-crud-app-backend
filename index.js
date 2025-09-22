af23ac7ee7108d25c8eef90e0cc1909072d1d716
Ephraim-blessing-mwereza <blessingmwereza@gmail.com>
2025-09-23 02:39:30 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// Using Node.js `require()`
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js")
+const app = express();
+
+// middlewhereconfig
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// routes
+app.use("/api/products", productRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server updated");
+});
+
+
+mongoose
+  .connect(
+    "mongodb+srv://mwereza:Melchizedeck25@backenddb.ryoaqrv.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB"
+  )
+  .then(() => {
+    console.log("Connected to the database!");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("connection failed!");
+  });
